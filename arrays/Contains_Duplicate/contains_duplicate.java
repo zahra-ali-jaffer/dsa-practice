8c60cebfec306c25df11fee6842a62bc5d2e4062
Zahra Ali Jaffer <zahramuraj@outlook.com>
2025-12-25 22:53:56 +0000

    Create contains_duplicate.java

---
+++ arrays/Contains_Duplicate/contains_duplicate.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
