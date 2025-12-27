03c67f3cefb5abea5cca4919df3a6617e972b6db
Zahra Ali Jaffer <zahramuraj@outlook.com>
2025-12-27 10:46:37 +0000

    Create valid_anagram.java

---
+++ arrays/ValidAnagram/valid_anagram.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+class ValidAnagram {
+    static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+        char[] a = s.toCharArray();
+        char[] b = t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a, b);
+    }
+}
