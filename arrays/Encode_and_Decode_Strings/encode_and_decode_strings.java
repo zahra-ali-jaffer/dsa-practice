990bb11adcb8645a21fb7e9ab0afef0d8b6d8817
Zahra Ali Jaffer <zahramuraj@outlook.com>
2026-01-19 13:30:27 +0000

    Update encode_and_decode_strings.java

--- arrays/Encode_and_Decode_Strings/encode_and_decode_strings.java
+++ arrays/Encode_and_Decode_Strings/encode_and_decode_strings.java
@@ -11,14 +11,21 @@ public class Solution {
     List<String> res = new ArrayList<>();
     int i = 0;
     while (i < str.length()) {
-      int j = i;
+      int j = i; // Find the '#'
       while (str.charAt(j) != '#'){
         j++;
       }
+      //Get the length
       int length = Integer.parseInt(str.substring(i, j));
+
+      // Move i to the start of the actual string
       i = j + 1;
+
+      // Extract the string of given len
       j = i + length;
       res.add(str.substring(i, j));
+
+      // Move to the next encoded part
       i = j;
     }
     return res;
