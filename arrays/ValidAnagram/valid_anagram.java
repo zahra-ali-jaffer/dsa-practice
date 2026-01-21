236e35fbb0eddd29a8fade068234802e2584bb17
Zahra Ali Jaffer <zahramuraj@outlook.com>
2026-01-21 04:15:59 +0000

    Update valid_anagram.java

--- arrays/ValidAnagram/valid_anagram.java
+++ arrays/ValidAnagram/valid_anagram.java
@@ -1,12 +1,12 @@
-import java.util.Arrays;
+import java.util.Arrays; //Imports the Arrays class, which provides utility methods like sort() and equals().
 
 class ValidAnagram {
-    static boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        char[] a = s.toCharArray();
+    static boolean isAnagram(String s, String t) { //A static method that takes two strings s and t. Returns true if they are anagrams; Returns false otherwise
+        if (s.length() != t.length()) return false; //Length check, cannot be anagrams if not same
+        char[] a = s.toCharArray(); //Convert strings to character arrays
         char[] b = t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a, b);
+        Arrays.sort(a); //Sort
+        Arrays.sort(b); //If two strings are anagrams, their sorted character arrays will be identical.
+        return Arrays.equals(a, b); 
     }
 }
