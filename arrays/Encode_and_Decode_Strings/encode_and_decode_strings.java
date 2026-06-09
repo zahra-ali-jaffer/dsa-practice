af72b879c035fad3cfcf0d81a4cc22d19ec8d25a
Zahra Ali Jaffer <zahramuraj@outlook.com>
2026-06-09 20:49:35 +0100

    Update encode_and_decode_strings.java

--- arrays/Encode_and_Decode_Strings/encode_and_decode_strings.java
+++ arrays/Encode_and_Decode_Strings/encode_and_decode_strings.java
@@ -1,6 +1,6 @@
 public class Solution {
 
-  public String encode(List<String> strs){
+  public String encode(List<String> strs){ 
     StringBuilder res = new StringBuilder();
     for (String s : strs) {
       res.append(s.length()).append('#').append(s);
