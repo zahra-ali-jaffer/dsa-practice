76e602acf9d6abcf7c774eb1b959bcf06e5ed540
Zahra Ali Jaffer <zahramuraj@outlook.com>
2026-07-13 18:53:01 +0100

    Update contains_duplicate.java

--- arrays/Contains_Duplicate/contains_duplicate.java
+++ arrays/Contains_Duplicate/contains_duplicate.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution { 
     public boolean hasDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; i++) { //This loop selects each element in the array one by one.
             for (int j = i + 1; j < nums.length; j++) { //compares the current element nums[i] with every element that comes after it.
